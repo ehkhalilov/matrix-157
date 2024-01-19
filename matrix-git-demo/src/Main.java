e538e28372a16834ac7aed82359dbd32b0341499
Azar-R <javalearning24@gmail.com>
2024-01-19 19:55:59 +0400

    3cu deyisiklik

--- matrix-git-demo/src/Main.java
+++ matrix-git-demo/src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Matrix!");
         System.out.println("Hello Test!");
+        System.out.println("this is feature 3");
+        //by Azar
     }
 }
