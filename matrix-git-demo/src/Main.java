7ff310e5cccb5a94db8a283459690c21fbdd7906
huaseynzade <huaseynzade@gmail.com>
2024-01-22 19:26:15 +0400

    YYY added

--- matrix-git-demo/src/Main.java
+++ matrix-git-demo/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Test!");
         System.out.println("this is feature 3");
+        System.out.printf("yyy");
         //by Azar
     }
 }
